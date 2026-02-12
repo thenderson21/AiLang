c16ae13323fefe693fe7b676178abb558771c00e
Todd Henderson <todd@todd-henderson.me>
2026-02-12 12:00:13 -0600

    Move AosRuntime into dedicated core file

--- src/AiLang.Core/AosInterpreter.cs
+++ src/AiLang.Core/AosInterpreter.cs
@@ -1,21 +1,4 @@
-using AiVM.Core;
-
 namespace AiLang.Core;
-
-public sealed class AosRuntime
-{
-    public Dictionary<string, AosValue> Env { get; } = new(StringComparer.Ordinal);
-    public HashSet<string> Permissions { get; } = new(StringComparer.Ordinal) { "math" };
-    public HashSet<string> ReadOnlyBindings { get; } = new(StringComparer.Ordinal);
-    public string ModuleBaseDir { get; set; } = HostFileSystem.GetFullPath(".");
-    public Dictionary<string, Dictionary<string, AosValue>> ModuleExports { get; } = new(StringComparer.Ordinal);
-    public HashSet<string> ModuleLoading { get; } = new(StringComparer.Ordinal);
-    public Stack<Dictionary<string, AosValue>> ExportScopes { get; } = new();
-    public bool TraceEnabled { get; set; }
-    public List<AosNode> TraceSteps { get; } = new();
-    public AosNode? Program { get; set; }
-    public VmNetworkState Network { get; } = new();
-}
 
 public sealed partial class AosInterpreter
 {
